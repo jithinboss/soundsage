f469a259ae2ddd3f95c7ea1c6de69d6ccb69fff4
jithinbose <jithinboss@gmail.com>
2023-10-20 22:48:11 +0530

    JB 1

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,11 @@
 import React, { memo, useState, useEffect, useMemo } from 'react';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
+import { Card, ListGroup, Dropdown } from 'react-bootstrap';
 //Icons
 //Components
 //data
-import { SubscriptionsServices } from "../../services/Subscriptions"
+
 
 interface Requests {
 
@@ -28,22 +29,59 @@ interface IHome {
 const Home: React.FC<IHome> = (props) => {
 
 
-	const [subscriptionsRequests, setSubscriptionsRequests] = useState<Requests[]>([]);
-
-	// useEffect(() => {
-	// 	subscriptionsRequests.getSubscriptionsRequests().then(data => setSubscriptionsRequests(data));
-	// }, []);
-
 	return (
 		<>
-			<div className="content-area content-area__bots">
-				<div className="card">
-					<DataTable value={subscriptionsRequests} tableStyle={{ minWidth: '50rem' }}>
-						<Column field="code" header="Code"></Column>
-						<Column field="name" header="Name"></Column>
-						<Column field="category" header="Category"></Column>
-						<Column field="quantity" header="Quantity"></Column>
-					</DataTable>
+			<div className="content-area home-page">
+				<div className="recent-courses">
+					<Card>
+						<Card.Body>
+							<div className='card-title'>
+								<h6>Recent Courses</h6>
+							</div>
+							<ListGroup className='course-list'>
+								<ListGroup.Item >
+									<span className='name'><a>Course 1</a>(Violin)</span>
+									<span className='date'>12 Aug 2023</span>
+								</ListGroup.Item>
+								<ListGroup.Item >
+									<span className='name'><a>Course 1</a>(Violin)</span>
+									<span className='date'>12 Aug 2023</span>
+								</ListGroup.Item>
+								<ListGroup.Item >
+									<span className='name'><a>Course 1</a>(Violin)</span>
+									<span className='date'>12 Aug 2023</span>
+								</ListGroup.Item>
+							</ListGroup>
+						</Card.Body>
+					</Card>
+				</div>
+				<div className="subscription-chart">
+					<Card>
+						<Card.Body>
+							<div className='card-title'>
+								<h6>Courses Subscription</h6>
+							</div>
+							<Dropdown>
+								<Dropdown.Toggle variant="dropdown basic">
+									Dropdown Button
+								</Dropdown.Toggle>
+								<Dropdown.Menu>
+									<Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+									<Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+									<Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+								</Dropdown.Menu>
+							</Dropdown>
+						</Card.Body>
+					</Card>
+				</div>
+				<div className="subscription-requests">
+					<Card>
+						<Card.Body>
+							<div className='card-title'>
+								<h6>Subscription Requests</h6>
+							</div>
+						</Card.Body>
+					</Card>
 				</div>
 			</div>
 		</>
